9cc665daa84d2139babc28e866190d81b7e244d6
Arif1917 <mdarifhosen1917@gmail.com>
2025-04-19 17:59:31 +0600

    project overview

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+     <div data-theme="light">
+        <Navbar></Navbar>
+     </div>
+    </>
+  )
+}
+
+export default App
